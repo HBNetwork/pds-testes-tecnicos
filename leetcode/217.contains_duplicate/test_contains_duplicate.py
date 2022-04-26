95f231e9a2bb0288daecff8d06967cf373a04d4a
Francisco Alface <francisco.alface.br@avenuecode.com>
2022-04-26 20:08:41 -0300

    Solução do desafio 217 do Leetcode - Contains Duplicate

---
+++ leetcode/217.contains_duplicate/test_contains_duplicate.py
@@ -0,0 +1,14 @@
+import pytest
+from contains_duplicate import contains_duplicate
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([1, 2, 3, 1], True),
+        ([1, 2, 3, 4], False),
+        ([1, 1, 1, 3, 3, 4, 3, 2, 4, 2], True),
+    ],
+)
+def test_contains_duplicate(nums, expected):
+    assert expected == contains_duplicate(nums)
